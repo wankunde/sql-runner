835432512ba01ebef1a9461d6e81745c8fe3634a
wankunde <wankunde@163.com>
2021-03-09 12:06:31 +0800

    initial commit

--- src/main/scala/org/apache/sql/runner/Alert.scala
+++ src/main/scala/org/apache/sql/runner/Alert.scala
@@ -1,7 +1,8 @@
 // Copyright 2019 Leyantech Ltd. All Rights Reserved.
 package org.apache.sql.runner
 
-import org.apache.spark.sql.util.{DQUtil, DingTalkUtil}
+import org.apache.spark.sql.util.{DQUtil, DingTalkUtil, SystemVariables}
+import org.apache.sql.runner.container.ConfigContainer
 
 /**
  * 对非测试，运行失败的程序进行告警
@@ -13,7 +14,9 @@ object Alert extends ArgParser {
   def main(args: Array[String]): Unit = {
     if (!args.contains("--test") && !args.contains("--dryrun")) {
       parseArgument(args)
-      val alertMessage = s"程序 ${args(0)} ${args(1)} 运行失败，请检查！"
+      val env = ConfigContainer.getOrElse(SystemVariables.ENV, System.getenv(SystemVariables.ENV))
+
+      val alertMessage = s"$env : 程序 ${args(0)} 运行失败，请检查！"
       DingTalkUtil.markDownMessage(DQUtil.serverUrl, DQUtil.title, Seq(alertMessage))
     }
   }
